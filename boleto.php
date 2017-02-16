1f24cca515f337240f5e438fc66f1db0ebc61452
Felipe Rodríguez <f.rodriguez06@ufromail.cl>
2017-02-15 22:52:04 -0300

    Cambios en JS
    Cambios en JS

--- boleto.php
+++ boleto.php
@@ -15,48 +15,49 @@
 
 <div id="container">
 
-    <div id="container">
-        <nav class="nav has-shadow" id="top">
-            <div class="container">
-                <div class="nav-left" id="barraSuperior">
-                    <p>Sistema de Venta de Pasajes</p>
-                </div>
-                <span class="nav-toggle">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </span>
-            </div>
-        </nav>
-
-    <div id="boleto" style="width: 50px; height: auto;">
-        <script src="js/boleto.js"></script>
-        <table id="contenidoBoleto" style="width: 50px; margin-left: 15px; font-size: 13pt;">
-            <tr>
-                <th>
-                    <img src="img/Logo_1.png" width="400" />
-                </th>
-            </tr>
-        </table>
+<nav class="nav has-shadow" id="top">
+    <div class="container">
+        <div class="nav-left" id="barraSuperior">
+            <p>Sistema de Venta de Pasajes</p>
+        </div>
+        <span class="nav-toggle">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
     </div>
+</nav>
 
-    <form action="rest/ingreso_pasaje.php" method="get">
-        <?php
-            $idServicio = $_GET["idServicio"];
-            $asiento = $_GET["asiento"];
-            $idSalon = $_GET["idSalon"];
+<div id="boleto" style="width: 50px; height: auto;">
+    <script src="js/boleto.js"></script>
 
-        echo "<input type='hidden' name='idServicio' value='".$idServicio."'>";
-        echo "<input type='hidden' name='asiento' value='".$asiento."'>";
-        echo "<input type='hidden' name='idSalon' value='".$idSalon."'>";
-        ?>
+    <table id="contenidoBoleto" style="width: 50px; margin-left: 15px; font-size: 13pt;">
+        <tr>
+            <th>
+                <img src="img/Logo_1.png" width="400" />
+            </th>
+        </tr>
 
-        <input type="submit" class="button is-primary" onclick="imprimir();" value="Confirmar e Imprimir">
-        <a href="javascript:window.history.back();" class="button is-danger">Volver</a>
-    </form>
+    </table>
 
-    </div>
 </div>
 
+
+<form action="rest/ingreso_pasaje.php" method="get">
+    <?php
+        $idServicio = $_GET["idServicio"];
+        $asiento = $_GET["asiento"];
+        $idSalon = $_GET["idSalon"];
+
+    echo "<input type='hidden' name='idServicio' value='".$idServicio."'>";
+    echo "<input type='hidden' name='asiento' value='".$asiento."'>";
+    echo "<input type='hidden' name='idSalon' value='".$idSalon."'>";
+    ?>
+
+    <input type="submit" class="button is-primary" onclick="imprimir();" value="Confirmar e Imprimir">
+    <a href="javascript:window.history.back();" class="button is-danger">Volver</a>
+</form>
+
+</div>
 </body>
 </html>
